6ae3ee8762b7a445dc4a5973b8f4523503cfba19
Kashif <gr8.abbasi@gmail.com>
2017-11-12 18:34:23 +0100

    more closure with reset values

--- 05_closure/03/main.go
+++ 05_closure/03/main.go
@@ -4,9 +4,13 @@ import "fmt"
 
 // Closure with anonymus function Variable Type
 func main() {
-	var increment = wrapper()
+	increment := wrapper()
 	fmt.Println(increment())
 	fmt.Println(increment())
+	// Returns new function, hence new scope with new copy of x!
+	increment = wrapper()
+	fmt.Println(increment()) // 1
+	fmt.Println(increment()) // 2
 }
 
 func wrapper() func() int {
