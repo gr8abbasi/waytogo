9560a0d70699f371c14fb7dc28419adfc346d58f
Kashif <gr8.abbasi@gmail.com>
2017-11-21 22:40:20 +0100

    updated different ways to implement Sort

--- 12_interfaces/02/sort/01/main.go
+++ 12_interfaces/02/sort/01/main.go
@@ -5,23 +5,33 @@ import (
 	"sort"
 )
 
+// Implements Interface interface of Sort Package
 type people []string
+
+func (a people) Len() int           { return len(a) }
+func (a people) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a people) Less(i, j int) bool { return a[i] < a[j] }
 
 func main() {
 	// Slice of String of Type People
 	studyGroup := people{"Zeno", "John", "Al", "Jenny"}
-	sort.Strings(studyGroup)
+	// sort.Strings(studyGroup)
+	sort.Sort(studyGroup)
 	fmt.Println(studyGroup)
 
 	// Slice of String
 	students := []string{"Zeno", "John", "Al", "Jenny"}
-	sort.Strings(students)
+	//sort.Sort(sort.StringSlice(students))
+	sort.StringSlice(students).Sort()
+	//sort.Strings(students)
 	fmt.Println("-------------------------------------")
 	fmt.Println(students)
 
 	// Slice of Integer
 	integers := []int{7, 4, 8, 2, 9, 19, 12, 32, 3}
-	sort.Ints(integers)
+	//sort.Ints(integers)
+	//sort.IntSlice.Sort(integers)
+	sort.Sort(sort.IntSlice(integers))
 	fmt.Println("-------------------------------------")
 	fmt.Println(integers)
 
