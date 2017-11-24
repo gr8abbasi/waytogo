f855ad94da09d79eb64a5a58049e5b3a1ca0cb7e
Muhammad Kashif Abbasi <gr8.abbasi@gmail.com>
2017-11-24 16:42:42 +0100

    formatted

--- 14_channels/N-to-1/main.go
+++ 14_channels/N-to-1/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	c := make(chan int)
 	var wg sync.WaitGroup
-	wg.Add(2)
+	wg.Add(2) // if wg.Add(1) used inside routines it creates -race condition
 
 	go func() {
 		for i := 0; i < 10; i++ {
@@ -16,16 +16,19 @@ func main() {
 		}
 		wg.Done()
 	}()
+
 	go func() {
 		for i := 0; i < 10; i++ {
 			c <- i
 		}
 		wg.Done()
 	}()
+
 	go func() {
 		wg.Wait()
 		close(c)
 	}()
+
 	for n := range c {
 		fmt.Println(n)
 	}
