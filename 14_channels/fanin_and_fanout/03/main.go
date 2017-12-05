bf98fd1b4f67d885d897d8fe74285d8ea21e7356
Kashif <gr8.abbasi@gmail.com>
2017-12-05 21:07:35 +0100

    fixed issues with fanin and fanout

--- 14_channels/fanin_and_fanout/03/main.go
+++ 14_channels/fanin_and_fanout/03/main.go
@@ -2,61 +2,80 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
-	gen := gen(1000)
-	// out := make(chan int)
-	for c := range factorial(gen) {
+	input := gen()
+
+	// FANOUT, multiple workers working on same channel
+	c0 := factorial(input)
+	c1 := factorial(input)
+	c2 := factorial(input)
+	c3 := factorial(input)
+	c4 := factorial(input)
+	c5 := factorial(input)
+	c6 := factorial(input)
+	c7 := factorial(input)
+	c8 := factorial(input)
+	c9 := factorial(input)
+
+	for c := range merge(c0, c1, c2, c3, c4, c5, c6, c7, c8, c9) {
 		fmt.Println(c)
 	}
 }
 
-func gen(n int) <-chan int64 {
-	out := make(chan int64)
+func gen() <-chan int {
+	out := make(chan int)
 	go func() {
-		for index := 0; index < n; index++ {
-			out <- int64(index)
+		for i := 0; i < 100000; i++ {
+			for index := 3; index < 13; index++ {
+				out <- index
+			}
 		}
 		close(out)
 	}()
 	return out
 }
 
-func factorial(n <-chan int64) <-chan int64 {
-	out := make(chan int64)
-	for c := range n {
-		go func(num int64) {
-			var total int64
-			total = 1
-			for index := num; index > 0; index-- {
-				total *= index
-			}
-			out <- total
-		}(c)
-	}
+func factorial(input <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range input {
+			out <- fact(n)
+		}
+		close(out)
+	}()
 	return out
 }
 
-// func merge(cs <-chan int) <-chan int {
-// 	out := make(chan int)
-// 	var wg sync.WaitGroup
-// 	wg.Add(len(n))
-// 	for c := range n {
-// 		go func(num int) {
-// 			total := 1
-// 			for index := num; index > 0; index-- {
-// 				total *= index
-// 			}
-// 			out <- total
-// 			wg.Done()
-// 		}(c)
-// 	}
+func fact(n int) int {
+	total := 1
+	for i := n; i > 0; i-- {
+		total *= i
+	}
+	return total
+}
 
-// 	go func() {
-// 		wg.Wait()
-// 		close(out)
-// 	}()
+func merge(cs ...<-chan int) <-chan int {
+	out := make(chan int)
+	var wg sync.WaitGroup
 
-// 	return out
-// }
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	for _, c := range cs {
+		go output(c)
+	}
+
+	wg.Add(len(cs))
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
